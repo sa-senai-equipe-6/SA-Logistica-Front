0915296e9ecdad8f8bd85b938f71a9440415c6f8
Yago Matheus <yagofx@hotmail.com>
2022-07-14 19:42:35 -0300

    Criado esqueleto do projeto FrontEnd

---
+++ src/main/java/br/senai/logistica/frontend/InitApp.java
@@ -0,0 +1,15 @@
+package br.senai.logistica.frontend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class InitApp {
+
+	public static void main(String[] args) {
+		SpringApplicationBuilder sb = new SpringApplicationBuilder(InitApp.class);
+		sb.headless(false);
+		sb.build(args);
+	}
+
+}
