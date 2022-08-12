5b0582b7e035aeb2deb1c0eb90ec293d037525c3
Yago Matheus <yagofx@hotmail.com>
2022-08-12 18:44:22 -0300

    Removido teste desnecessario

--- src/test/java/br/senai/logistica/frontend/InitAppTests.java
+++ src/test/java/br/senai/logistica/frontend/InitAppTests.java
@@ -3,11 +3,11 @@ package br.senai.logistica.frontend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InitAppTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
